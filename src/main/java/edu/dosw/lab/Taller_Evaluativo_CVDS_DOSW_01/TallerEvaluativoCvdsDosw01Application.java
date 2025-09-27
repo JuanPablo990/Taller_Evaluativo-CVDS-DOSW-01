22b3e2543430d350edd46adf32778700f5112dac
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-09-26 20:07:13 -0500

    chore(file): se a agregado la configuracion de maven

---
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/TallerEvaluativoCvdsDosw01Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativoCvdsDosw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativoCvdsDosw01Application.class, args);
+	}
+
+}
