61c81f3e9b4e366da0d4cb03871c7ac868fa2b23
RaquelSAyala <142960772+RaquelSAyala@users.noreply.github.com>
2025-09-27 12:20:45 -0500

    feat(builder): creación de la rama builder

---
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/Report.java
@@ -0,0 +1,4 @@
+package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain;
+
+public class Report {
+}
---
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/builder/ReportBuilder.java
@@ -0,0 +1,4 @@
+package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.builder;
+
+public class ReportBuilder {
+}
