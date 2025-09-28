f9f7a1469fb9169f685c029d52f038dd97870f9a
RaquelSAyala <142960772+RaquelSAyala@users.noreply.github.com>
2025-09-27 22:35:55 -0500

    feat(Transaction): Código Transaction

--- src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/Transaction.java
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/Transaction.java
@@ -1,17 +1,6 @@
-package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.builder;
+package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain;
 
-import edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.Report;
-import edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.Transaction;
-
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
-public interface ReportBuilder {
-    ReportBuilder title(String title);
-    ReportBuilder generatedDate(LocalDate date);
-    ReportBuilder author(String author);
-    ReportBuilder transactions(List<Transaction> txs);
-    ReportBuilder content(String content);
-    ReportBuilder feature(String feature);
-    Report build();
-}
+public record Transaction(String id, LocalDate date, String description, BigDecimal amount) {}
