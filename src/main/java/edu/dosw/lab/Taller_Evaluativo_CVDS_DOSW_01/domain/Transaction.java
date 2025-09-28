ea294ed9b907530fd4a943e5e166a3a5ad8444f3
RaquelSAyala <142960772+RaquelSAyala@users.noreply.github.com>
2025-09-27 22:20:11 -0500

    feat(builder): codigo de ReportBuilder

--- src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/Transaction.java
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_CVDS_DOSW_01/domain/Transaction.java
@@ -1,4 +1,17 @@
-package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain;
+package edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.builder;
 
-public class Transaction {
+import edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.Report;
+import edu.dosw.lab.Taller_Evaluativo_CVDS_DOSW_01.domain.Transaction;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReportBuilder {
+    ReportBuilder title(String title);
+    ReportBuilder generatedDate(LocalDate date);
+    ReportBuilder author(String author);
+    ReportBuilder transactions(List<Transaction> txs);
+    ReportBuilder content(String content);
+    ReportBuilder feature(String feature);
+    Report build();
 }
